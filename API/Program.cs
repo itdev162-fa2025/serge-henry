69d07761070180bcbab4670bc98e4f2020cc5178
sergehen <sergehenry948@gmail.com>
2024-09-17 16:10:22 -0500

    Complete Activity 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,16 @@
+
+
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
-builder.services.AddControllers();
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContex>();
 
 var app = builder.Build();
 
@@ -16,5 +22,5 @@ if (app.Environment.IsDevelopment())
 }
 
 //app.UseHttpsRedirection();
-app.Mapcontrollers();
+app.MapControllers();
 app.Run();
