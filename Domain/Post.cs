df02173cb64f4a2e4403750092eadf45e3b2f359
Serge Henry <sergehenry948@gmail.com>
2024-10-15 15:11:26 -0500

    activity-5 changes have been made

--- Domain/Post.cs
+++ Domain/Post.cs
@@ -1,8 +1,8 @@
 namespace Domain{
  public class Post{
   public Guid Id{ get; set;}
-  public string Title {get; set;}
-  public string Body {get; set;}
+  public required string Title {get; set;}
+  public required string Body {get; set;}
   public DateTime Date {get; set;}
  }
 }
